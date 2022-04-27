9fb6d61d77f6cc5070b696503eed5d0c42c63d58
Iago Quintero <iagoquintero@letsbit.io>
2022-04-27 16:56:45 -0300

    cleanup

--- practica1/dos/dos.c
+++ practica1/dos/dos.c
@@ -8,45 +8,12 @@
 #define MAX_LINE 4096
 #define MAX_ARGS 2048
 
-char *aux(char *str, int l, int r)
-{
-  char *rta = malloc(r - l + 1);
-  for (int i = l; i < r; i++)
-  {
-    rta[i - l] = str[i];
-  }
-  rta[r - l] = '\0';
-  return rta;
-}
-
-char **extractCommand(char *input){
-  char **list = malloc(sizeof(char *) * MAX_ARGS);
-  int l = 0, r = 0, c = 0, i;
-
-  if (input[0] == ' '){
-    for(l = 0; input[l] == ' ';l++);
-  }
-
-  for (i = l; input[i] != '\0'; i++)
-  {
-    if (input[i] == ' ' || input[i] == '\n')
-    {
-      r = i;
-      list[c] = aux(input, l, r);
-      l = i + 1;
-      c++;
-    }
-  }
-  list[i++] = NULL;
-  return list;
-}
-
 void removeNewline(char *input){
   int i;
   for (i = 0;  input[i] != '\n' && input[i] != '\0';  i++);
   input[i] = '\0';
 }
-char **extractCommand2(char *input){
+char **extractCommand(char *input){
   char **tokens = malloc(sizeof(char*) * MAX_ARGS);
 
   char *token, *str, *tofree;
@@ -110,8 +77,8 @@ int main(int argc, char **argv)
   {
 
     char *rest;
-    char **firstArgs = extractCommand2(strtok_r(input, "|", &rest));
-    char **secondArgs = extractCommand2(strtok_r(NULL, "|", &rest));
+    char **firstArgs = extractCommand(strtok_r(input, "|", &rest));
+    char **secondArgs = extractCommand(strtok_r(NULL, "|", &rest));
 
     pid_t pid = fork();
     if (pid < 0)

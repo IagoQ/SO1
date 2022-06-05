3c47c654a340d04c4558cd4bf559daa1ac43b7bd
Iago Quintero <iagoquintero@letsbit.io>
2022-06-05 20:30:45 -0300

    better test

--- practica4/cinco/test.erl
+++ practica4/cinco/test.erl
@@ -7,13 +7,21 @@
 
 test() -> 
 	BPid = spawn(broadcaster,server,[]),
-	subscribe(BPid,10),
-	BPid ! {send, "ji"}.
+	Subs = subscribe(BPid,10,[]),
+	BPid ! {send, "ji"},
+	timer:sleep(1),
+	unsubscribe(BPid,Subs),
+	BPid ! {send, "je"}.
 
 
 
-subscribe(Broadcaster_pid, 0) -> ok;
-subscribe(Broadcaster_pid,N) -> 
+subscribe(Broadcaster_pid, 0, Subbed) -> Subbed;
+subscribe(Broadcaster_pid,N, Subbed) -> 
 	CPid = spawn(client,client,[]),
 	Broadcaster_pid ! {subscribe, CPid},
-	subscribe(Broadcaster_pid,N-1).
+	subscribe(Broadcaster_pid,N-1, [CPid|Subbed]).
+
+unsubscribe(Broadcaster_pid,[]) -> ok;
+unsubscribe(Broadcaster_pid,[Sub|Subbed]) -> 
+	Broadcaster_pid ! {unsubscribe,Sub},
+	unsubscribe(Broadcaster_pid,Subbed).

edab0c43a90fc43a23cd02c33f9312d56ed71793
Marina Belmonte <74318173+maribelmonte@users.noreply.github.com>
2022-06-09 15:24:06 -0300

    fix

--- practica4/ocho/hello.erl
+++ practica4/ocho/hello.erl
@@ -1,33 +1,33 @@
 -module(hello).
--export([init/0,hello/0]).
+-export([init/0,hello/0,printer/0]).
 -on_load(reload/0).
 
 hello() ->
     process_flag(trap_exit, true),
     spawn_link(?MODULE, printer, []),
     receive
-	{'EXIT', From, Reason} -> spawn_link(?MODULE, printer, [])
-    end,
+	     {'EXIT', From, Reason} -> spawn_link(?MODULE, printer, [])
+    end.
 
 printer() ->
-	case whereis(hellopid) of 
+	case whereis(printerpid) of
 		undefined -> register(printerpid, self());
 		_ -> unregister(printerpid), register(printerpid, self())
 	end,
 
-	receive 
-		upgrade -> 
+	receive
+		upgrade ->
 			io:fwrite("calling \n"),
 			spawn(?MODULE,printer,[]),
 			exit("reloaded")
 	after 1000 -> ok end,
-	io:fwrite("2 ~p~n", [case rand:uniform(10) of 10 -> uno; _ -> self() end]),
-	hello().
+	io:fwrite("6 ~p~n", [case rand:uniform(10) of 10 -> 1/uno; _ -> self() end]),
+	printer().
 
 init() -> spawn(fun () -> hello() end).
 
 reload() ->
-	case whereis(printerpid) of 
+	case whereis(printerpid) of
 		undefined -> ok;
 		Pid -> io:fwrite("reloading \n"),Pid ! upgrade, ok
 	end.
